7e7cb28ee5bdcd30d99a11ccefe35a2eebddaa28
Nick Chang <nick.chang@serimaconsulting.com>
2019-03-28 11:03:01 -0400

    life cycle

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Messages from './Messages';
+import TheDate from './state/TheDate';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>YOUR APPLICATION NAME!</h1>
-        <Messages name="Messages" unread={0} />
-        <Messages name="Notifications" unread={10} />
-      </div>
+      <TheDate />
     );
   }
 }
